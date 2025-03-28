77aa0fc63da24f9927a6c406f3954ad994d2e070
ignaceeine <ignaceeine@gmail.com>
2025-03-28 15:55:47 +0000

    Authentication add

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,17 +3,32 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-5">
-                <a href="{{ route('index') }}"><img src="{{ URL::asset('build/img/logo-paraclet.png') }}" alt="" width="125" height="65"></a>
+                <a href="{{ route('index') }}"><img src="{{ URL::asset('img/logo-paraclet.png') }}" alt="" width="125" height="65"></a>
             </div>
             <div class="col-7">
                 <div id="social">
                     <ul>
                         <li>
-                            <button type="button" class="btn btn-circle bg-primary-subtle" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                                <i class="bi bi-person-circle text-primary fs-4"></i>
-                            </button>
+                            @if(Auth::user())
+                                <div class="dropdown">
+                                    <button class="btn btn-circle bg-primary-subtle dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="bi bi-person-circle text-primary fs-4"></i>
+                                    </button>
+                                    <ul class="dropdown-menu bg-primary-subtle" aria-labelledby="userDropdown">
+                                        <li class="text-primary ms-3">{{ Auth::user()->prenom }}</li>
+                                        <li><hr class="dropdown-divider"></li>
+                                        <li class="bg-danger w-100">
+                                            <form method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                            <a class="dropdown-item bg-danger" href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();">Déconnexion</a>
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </div>
+                            @else
+                                <a href="{{ route('login') }}" class="btn btn-primary">connexion</a>
+                            @endif
                         </li>
-
                     </ul>
                 </div>
             </div>
--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -35,10 +35,10 @@
     </div>
 @endsection
 @section('scripts')
-    <script src="{{ URL::asset('build/js/jquery-3.7.1.min.js') }}"></script>
-    <script src="{{ URL::asset('build/js/common_scripts.min.js') }}"></script>
-    <script src="{{ URL::asset('build/js/functions.js') }}"></script>
-    <script src="{{ URL::asset('build/js/survey_func.js') }}"></script>
+    <script src="{{ URL::asset('js/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ URL::asset('js/common_scripts.min.js') }}"></script>
+    <script src="{{ URL::asset('js/functions.js') }}"></script>
+    <script src="{{ URL::asset('js/survey_func.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 @endsection
